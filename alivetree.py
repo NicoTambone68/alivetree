512736362a76affd8a5e9398848f1c39535a98df
NicoTambone68 <nick@example.com>
2013-10-24 10:08:59 +0000

    now handles page-breaks

--- alivetree.py
+++ alivetree.py
@@ -29,7 +29,7 @@ import re
 import sys
 import getopt
 import zipfile
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 
 
 class OdtDocument():
@@ -51,9 +51,10 @@ class OdtDocument():
         self.style_fontweight = {}
         self.style_textunderline = {}
         self.style_textalignment = {}
+        self.style_break_before = {}
         # counter of the h<n> elements
         self.heading_id_count = 0
-        # contains h<n> element values (titles) 
+        # contains h<n> element values (titles)
         # index is by position starting from 1 (self.heading_list[0] = ["-", "-"])
         # each item is a list so formed [<"heading element">, <"heading level">]
         self.heading_list = [[unicode("-"), unicode("-")]]
@@ -76,13 +77,13 @@ class OdtDocument():
                     content = src.read(l.orig_filename)
                     fw.write(content)
                     fw.close()
-                # extract the metadata xml    
+                # extract the metadata xml
                 elif l.orig_filename == 'meta.xml':
                     fw = open('meta.xml', 'w')
                     meta = src.read(l.orig_filename)
                     fw.write(meta)
                     fw.close()
-                else:    
+                else:
                     pass
 
 
@@ -92,38 +93,47 @@ class OdtDocument():
 
     def outliner(self, str=unicode(""), outline_level=unicode(""), style_name=unicode("")):
         """
-         Encloses the unicode str between <p> or <h[n]> tags, 
+         Encloses the unicode str between <p> or <h[n]> tags,
          according to the given outline_level
 
         """
 
         paragraph_style = unicode("p-style-left")
 
+        break_before = unicode("")
+
         try:
             paragraph_style = unicode("p-style-%s" % self.style_textalignment[style_name])
-        
-        except:
-            pass                
 
-        if outline_level == "paragraph":            
-            if str:        
-                sret = unicode('<p class="%s">%s</p>' % (paragraph_style, str)) + os.linesep
+        except:
+            pass
+
+        try:
+                break_before = unicode("p-style-%s-break-before" % self.style_break_before[style_name])
+
+        except:
+            pass
+
+        if outline_level == "paragraph":
+            if str:
+                sret = unicode('<p class="%s %s">%s</p>' % (paragraph_style, break_before, str)) + os.linesep
             else:
-                sret = unicode("")                
+                sret = unicode('<p class="%s %s"></p>' % (paragraph_style, break_before)) + os.linesep
+
         else:
                 self.heading_id_count = self.heading_id_count + 1
                 s_id_cnt = unicode(self.heading_id_count)
                 sret = unicode('<h%s id="heading_id_%s">%s</h%s>' % (outline_level, s_id_cnt, str, outline_level)) + os.linesep
                 self.heading_list.append([unicode(str), unicode(outline_level)])
 
-        return sret 
-        
+        return sret
+
 
     def stylizer(self, str=unicode(""), style_name=unicode("")):
       """
        Place <i> or <b> ora <span style="..."> tags according to the current style_name
       """
-      sret = str     
+      sret = str
 
       try:
 
@@ -132,19 +142,19 @@ class OdtDocument():
 
       finally:
 
-        try:               
+        try:
 
           if self.style_fontweight[style_name] == "bold":
             sret = unicode("<b>%s</b>" % sret)
 
         finally:
 
-          try:        
-            
+          try:
+
             if self.style_textunderline[style_name] == "underlined":
               sret = unicode('<span style="text-decoration: underline;">%s</span>' % sret)
 
-          finally:        
+          finally:
                 return sret
 
 
@@ -153,7 +163,7 @@ class OdtDocument():
 
          Loads metadata info from meta.xml
          Those data include: title, author name, last save date
-         
+
         """
         handler = open('meta.xml').read()
         soup = BeautifulSoup(handler, 'xml')
@@ -165,7 +175,7 @@ class OdtDocument():
         for meta in soup.find_all(re.compile("^initial.creator")):
             if meta.string:
                 self.meta_initial_creator = unicode(meta.string)
-            
+
         for meta in soup.find_all(re.compile("^date")):
             if meta.string:
                 self.meta_date = unicode(meta.string)
@@ -176,7 +186,7 @@ class OdtDocument():
         Converts self.xml_doc to html (self.filename_out)
 
         """
-        handler = open(self.xml_doc).read()        
+        handler = open(self.xml_doc).read()
         soup = BeautifulSoup(handler, 'xml')
 
         fw = open(self.filename_out, 'w')
@@ -188,7 +198,7 @@ class OdtDocument():
         fw.write("<link rel=\"stylesheet\" href=\"%s\" type=\"text/css\" />" % self.stylesheet_name + os.linesep)
         fw.write("<title></title>" + os.linesep)
         fw.write("</head>" + os.linesep)
-        fw.write("<body>" + os.linesep)        
+        fw.write("<body>" + os.linesep)
 
         # Load styles in dictionaries
         for style in soup.find_all("style"):
@@ -198,109 +208,117 @@ class OdtDocument():
                 fs = style_child.get("fo:font-style")
                 if fs:
                     self.style_fontstyle[style_name] = fs
-                fontw = style_child.get("fo:font-weight")    
+                fontw = style_child.get("fo:font-weight")
                 if fontw:
                     self.style_fontweight[style_name] = fontw
-                # read alignment 
+                # read alignment
                 txta = style_child.get("fo:text-align")
                 if txta:
-                    self.style_textalignment[style_name] = txta 
-                # !!!    
-                tu = style_child.get("style:text-underline-type")    
-                if tu: 
+                    self.style_textalignment[style_name] = txta
+                # !!!
+                tu = style_child.get("style:text-underline-type")
+                if tu:
                     self.style_textunderline[style_name] = "underlined"
-       
+                # page break
+                break_before = style_child.get("fo:break-before")
+                if break_before:
+                    self.style_break_before[style_name] = break_before
+
 
         # Navigate down the document through h and p tags
-        # 
+        #
         for text in soup.find_all(re.compile("^h|^p")):
 
                    # From bs4 docs: If a tag has only one child, and that child is a NavigableString, the child is made available as .string:
                    # This covers the following case (e.g.):
                    #
-                   # <text:p text:style-name="P9">- Any text here!</text:p> 
+                   # <text:p text:style-name="P9">- Any text here!</text:p>
                    #
-                   # To do: 
+                   # To do:
                    #
                    # Beware of this case:
                    # - <text:p text:style-name="P8">
                    #     <text:span text:style-name="T4">
                    #
+
+                   # Get the attributes so the styles and the outlines
+                   text_attrs = dict(text.attrs)
+
+                   # Get the styles, if any
+                   try:
+                       t_style = text_attrs["text:style-name"]
+                   except:
+                       t_style = "nostyle"
+
+                   # Get the outline-levels, if any
+                   try:
+                       t_outline_level = text_attrs["text:outline-level"]
+                   except:
+                       t_outline_level = "paragraph"
+
                    if text.string:
+                       t = unicode(text.string)
+                       if t:
+                           fw.write(self.outliner(self.stylizer(t, t_style), t_outline_level, t_style).encode('utf-8'))
 
-                        # Get the attributes so the styles and the outlines
-                        text_attrs = dict(text.attrs)
-                        
-                        # Get the styles, if any
-                        try:
-                           t_style = text_attrs["text:style-name"]
-                        except:
-                           t_style = "nostyle"            
-
-                        # Get the outline-levels, if any
-                        try:
-                           t_outline_level = text_attrs["text:outline-level"]
-                        except:   
-                           t_outline_level = "paragraph"
-
-                        t = unicode(text.string)
-                        if t:
-                            fw.write(self.outliner(self.stylizer(t, t_style), t_outline_level, t_style).encode('utf-8'))        
+                   # e.g. page breaks come as a node with no children whose style contains fo:break-before:"page"
+                   elif len(list(text.children)) == 0:
+                        fw.write(self.outliner(unicode(""), t_outline_level, t_style).encode('utf-8'))
 
                    # This covers the following case (e.g.):
-                   #     
+                   #
                    # <text:p text:style-name="Textbody">
-                   #  jkjksk skjkjkjs dhh 
-                   #  <text:s /> 
-                   #  <text:span text:style-name="T3">Bold</text:span> 
-                   #  <text:s /> 
-                   # </text:p>                  
-                   #   
-                   # else drill down one level    
+                   #  jkjksk skjkjkjs dhh
+                   #  <text:s />
+                   #  <text:span text:style-name="T3">Bold</text:span>
+                   #  <text:s />
+                   # </text:p>
+                   #
+                   # else drill down one level
                    else:
                        buffer = unicode("")
                        t = buffer
                        u = buffer
                        t_outline_level = "paragraph"
                        t_style = ""
-                       for i in text.children: 
+                       for i in text.children:
                            # Get the attributes so the styles
                            try:
-                               text_attrs = dict(i.attrs)                                   
+                               text_attrs = dict(i.attrs)
                                t_style = text_attrs["text:style-name"]
                            except:
                                # whenever the element has no style
                                # take the parent's one
-                               try:           
-                                   text_attrs = dict(i.parent.attrs) 
+                               try:
+                                   text_attrs = dict(i.parent.attrs)
                                    t_style = text_attrs["text:style-name"]
                                except:
-                                   t_style = "nostyle"            
+                                   t_style = "nostyle"
 
                            # Get the outline-levels, if any
                            try:
                                t_outline_level = text_attrs["text:outline-level"]
-                           except:   
+                           except:
                                t_outline_level = "paragraph"
 
                            # if the current tag has only one child, and that child is a NavigableString
                            if i.string:
                                t = unicode(i.string)
-                           
+
                            # space
                            elif i.name == "s":
                                t = unicode("&nbsp;")
 
-                           # else drill down another level    
+                           # else drill down another level
                            else:
-                               t = unicode("")           
+                               t = unicode("")
                                for j in i.children:
-                                   if j.string:               
+                                   if j.string:
                                        u = unicode(j.string)
                                    elif j.name == "s":
                                        u = unicode("&nbsp;")
                                    else:
-                                       u = unicode("")                                   
+                                       u = unicode("")
                                    if u:
                                        t = t + self.stylizer(u, t_style)
 
@@ -318,9 +336,9 @@ class OdtDocument():
 
     def content_opf(self):
         """
-         Writes content.opf 
+         Writes content.opf
         """
-        
+
         fw = open("content.opf", 'w')
         fw.write('<?xml version="1.0" encoding="UTF-8"?>' + os.linesep)
         fw.write('  <package xmlns="http://www.idpf.org/2007/opf" unique-identifier="BookID" version="2.0">' + os.linesep)
@@ -352,12 +370,12 @@ class OdtDocument():
         fw.close()
 
 
-    def toc_ncx(self):    
+    def toc_ncx(self):
         """
          Writes toc.ncx
         """
-       
-        fw = open("toc.ncx", "w") 
+
+        fw = open("toc.ncx", "w")
         fw.write('<?xml version="1.0"?>' + os.linesep)
         fw.write('<!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN"' + os.linesep)
         fw.write('    "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">' + os.linesep)
@@ -385,7 +403,7 @@ class OdtDocument():
         fw.write('  </navMap>' + os.linesep)
         fw.write('</ncx>' + os.linesep)
         fw.close()
-       
+
 
     def toc_html(self):
         """
@@ -400,13 +418,13 @@ class OdtDocument():
         fw.write("<link rel=\"stylesheet\" href=\"%s\" type=\"text/css\" />" % self.stylesheet_name + os.linesep)
         fw.write("<title>Table of contents</title>" + os.linesep)
         fw.write("</head>" + os.linesep)
-        fw.write("<body>" + os.linesep)        
+        fw.write("<body>" + os.linesep)
         fw.write("<h1>%s</h1>" % self.meta_title + os.linesep)
         fw.write("<div>" + os.linesep)
 
         for i in range(1, len(self.heading_list)):
             # to do: different styles according to heading level self.heading_list[i[1]]
-            fw.write("<p class=\"toc_level_%s\"><a href=\"%s#heading_id_%s\">%s</a></p>" % (self.heading_list[i][1], self.filename_out, unicode(i), self.heading_list[i][0]) + os.linesep)
+            fw.write("<p class=\"toc-level-%s\"><a href=\"%s#heading_id_%s\">%s</a></p>" % (self.heading_list[i][1], self.filename_out, unicode(i), self.heading_list[i][0]) + os.linesep)
 
         fw.write("</div>" + os.linesep)
         fw.write("</body>" + os.linesep)
@@ -441,46 +459,48 @@ class OdtDocument():
         fw.write("    text-align: left;" + os.linesep)
         fw.write("}" + os.linesep)
 
-        fw.write(".p-style-right {" + os.linesep)
+        fw.write(".p-style-end {" + os.linesep)
         fw.write("    text-align: right;" + os.linesep)
         fw.write("}" + os.linesep)
 
         fw.write(".p-style-justify {" + os.linesep)
         fw.write("    text-align: justify;" + os.linesep)
-        fw.write("}" + os.linesep)        
+        fw.write("}" + os.linesep)
 
-        fw.write(".toc_level_1 {" + os.linesep)
+        fw.write(".p-style-page-break-before { page-break-before: always; }" + os.linesep)
+
+        fw.write(".toc-level-1 {" + os.linesep)
         fw.write("    text-indent: 0;" + os.linesep)
         fw.write("}" + os.linesep)
-        fw.write(".toc_level_2 {" + os.linesep)
+        fw.write(".toc-level-2 {" + os.linesep)
         fw.write("    text-indent: 2%;" + os.linesep)
         fw.write("}" + os.linesep)
-        fw.write(".toc_level_3 {" + os.linesep)
+        fw.write(".toc-level-3 {" + os.linesep)
         fw.write("    text-indent: 4%;" + os.linesep)
         fw.write("}" + os.linesep)
-        fw.write(".toc_level_4 {" + os.linesep)
+        fw.write(".toc-level-4 {" + os.linesep)
         fw.write("    text-indent: 6%;" + os.linesep)
         fw.write("}" + os.linesep)
-        fw.write(".toc_level_5 {" + os.linesep)
+        fw.write(".toc-level-5 {" + os.linesep)
         fw.write("    text-indent: 8%;" + os.linesep)
         fw.write("}" + os.linesep)
-        fw.write(".toc_level_6 {" + os.linesep)
+        fw.write(".toc-level-6 {" + os.linesep)
         fw.write("    text-indent: 10%;" + os.linesep)
         fw.write("}" + os.linesep)
         fw.close()
-        
+
 def noparam():
-    print "No parameters were given. At least you should tell me which one is the input file" 
+    print "No parameters were given. At least you should tell me which one is the input file"
 
 def usage():
-    print "usage: to do, sorry :-)"    
+    print "usage: to do, sorry :-)"
 
 def main(argv):
     try:
         for args in sys.argv:
                 opts, args = getopt.getopt(argv, "l:i:", ["lang=", "input="])
 
-    except getopt.GetoptError:            
+    except getopt.GetoptError:
         usage()
         sys.exit(2)
 
@@ -489,11 +509,11 @@ def main(argv):
 
     for opt, arg in opts:
         if opt in ("-l", "--lang"):
-            lang = arg                
-               
+            lang = arg
+
         elif opt in ("-i", "--input"):
-            odt_document = arg        
-        
+            odt_document = arg
+
         else:
             pass
 
@@ -507,8 +527,8 @@ def main(argv):
         if lang:
             odt.language = lang
             print "Document language=%s" % lang
-        else:        
-            print "WARNING: lang not specified; using default EN"            
+        else:
+            print "WARNING: lang not specified; using default EN"
         odt.dumpxml()
         odt.load_metadata()
         odt.xml2html()
@@ -518,7 +538,7 @@ def main(argv):
         odt.style_css()
         print "Done."
     except:
-        print "ERROR processing %s. Bad file or file corrupted" % odt_document                    
+        print "ERROR processing %s. Bad file or file corrupted" % odt_document
 
 
 if __name__ == "__main__":
